44ca42b1ce6268f670a9354c9415820839c97db2
Mr. Park <mrparkonline@gmail.com>
2019-05-27 13:25:56 -0400

    Added Collapsables fofor testing

--- app.py
+++ app.py
@@ -10,12 +10,24 @@
 # import lines
 from flask import Flask
 from flask import render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('home.html')
+
+'''
+Context Processor 'Injection of items'
+- We can give our Flask project new variables that are accesible in HTML
+- The function must return a dictionary
+'''
+@app.context_processor
+def getYear():
+	current = datetime.datetime.now()
+
+	return dict(year=str(current.year))
 
 # Alternative way to run via python3.
 if __name__ == '__main__':
